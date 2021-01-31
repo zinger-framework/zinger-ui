95be22fba0ae19d0d2224ea1473851d210757ddf
Logesh Dinakaran <ddlogesh@gmail.com>
2021-01-31 18:42:27 +0530

    Added angular-wizard

--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-
-
+import {ArchwizardModule} from 'angular-archwizard';
 
 @NgModule({
   declarations: [LoginComponent, ForgotPasswordComponent],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    ArchwizardModule
   ]
 })
 export class LoginModule { }
