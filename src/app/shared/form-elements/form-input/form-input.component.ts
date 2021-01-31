76247ec588c08ca1ef37e55f7e4719deaffb697e
Logesh Dinakaran <ddlogesh@gmail.com>
2021-02-01 02:46:10 +0530

    Completed FormInputComponent

--- src/app/shared/form-elements/form-input/form-input.component.ts
+++ src/app/shared/form-elements/form-input/form-input.component.ts
@@ -1,18 +1,24 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {ControlContainer, FormGroupDirective} from '@angular/forms';
 
 @Component({
   selector: 'form-input',
   templateUrl: './form-input.component.html',
-  styleUrls: ['./form-input.component.css']
+  styleUrls: ['./form-input.component.css'],
+  viewProviders: [
+    {
+      provide: ControlContainer,
+      useExisting: FormGroupDirective
+    }
+  ]
 })
 export class FormInputComponent implements OnInit {
-  @Input() inputType = 'text';
-  @Input() placeholder = '';
-  @Input() formControlName = this.inputType;
+  @Input() inputType: string;
+  @Input() placeholder: string;
+  @Input() name: string;
   @Input() isRequired = true;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 }
