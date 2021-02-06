f5af9dfe655270ef073ff600e7b9509462769559
Logesh Dinakaran <ddlogesh@gmail.com>
2021-02-07 01:08:13 +0530

    Removed unused data-binding code changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {JwtService} from './core/service/jwt.service';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,6 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'zinger-ui';
-
-  constructor() {
+  constructor(public jwtService: JwtService) {
   }
 }
--- src/app/layouts/public/public.component.ts
+++ src/app/layouts/public/public.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-public',
@@ -6,8 +6,6 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./public.component.css']
 })
 export class PublicComponent implements OnInit {
-  @Input() componentName: string;
-
   constructor() {
   }
 
