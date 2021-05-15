de83374c40c443012db553ace5bb56cda2e5d66e
harshavardhanp <harshavardhan.zodiac@gmail.com>
2021-05-16 01:45:54 +0530

    Changed Description input field

--- src/app/shared/form-elements/image-preview/image-preview.component.ts
+++ src/app/shared/form-elements/image-preview/image-preview.component.ts
@@ -10,11 +10,18 @@ export class ImagePreviewComponent implements OnInit {
 
   @Input() imageSrc: string = "https://images.unsplash.com/photo-1559028012-481c04fa702d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2ViJTIwZGVzaWdufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60";
   @Input() imageName: string = "";
+  @Input() imageType: string = "";
   @Output() deleteImageEvent = new EventEmitter<string>();
-
-  constructor(public toast: ToastrService) { }
+  height=100;
+  width=100;
+  constructor(public toast: ToastrService) {
+  }
 
   ngOnInit(): void {
+    if(this.imageType=='cover_photos'){
+      this.height=100;
+      this.width=200;
+    }
   }
 
   deleteImage(){
