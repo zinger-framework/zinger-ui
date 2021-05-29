fa7784ef95fa806782e2a441debfa97c149226d5
harshavardhanp <harshavardhan.zodiac@gmail.com>
2021-05-29 16:12:04 +0530

    Added comment component

---
+++ src/app/shared/comment/comment.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'comment',
+  templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.css']
+})
+export class CommentComponent implements OnInit {
+
+  @Input() name;
+  @Input() timeOfComment;
+  @Input() comment;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,13 +5,15 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {FormInputComponent} from './form-elements/form-input/form-input.component';
 import {CardComponent} from './card/card.component';
 import {ImagePreviewComponent} from './form-elements/image-preview/image-preview.component';
+import { CommentComponent } from './comment/comment.component';
 
 @NgModule({
-  declarations: [FormInputComponent, CardComponent, ImagePreviewComponent],
+  declarations: [FormInputComponent, CardComponent, ImagePreviewComponent, CommentComponent],
   exports: [
     FormInputComponent,
     CardComponent,
-    ImagePreviewComponent
+    ImagePreviewComponent,
+    CommentComponent
   ],
   imports: [
     CommonModule,
