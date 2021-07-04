f7d180b2e79cc3ec71d709f29e2e5da3e1026e72
harshavardhanp <harshavardhan.zodiac@gmail.com>
2021-07-05 02:54:22 +0530

    Fixed issues in reason-modal component

--- src/app/shared/reason-modal/reason-modal.component.ts
+++ src/app/shared/reason-modal/reason-modal.component.ts
@@ -2,18 +2,20 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ExtendedFormControl} from "../../core/utils/extended-form-control.utils";
+import {BaseComponent} from "../../base.component";
 
 @Component({
   selector: 'reason-modal',
   templateUrl: './reason-modal.component.html',
   styleUrls: ['./reason-modal.component.css']
 })
-export class ReasonModalComponent implements OnInit {
+export class ReasonModalComponent extends BaseComponent {
   @Input() title;
   @Output() updateStatus: EventEmitter<{ title: string, formObject: FormGroup }> = new EventEmitter();
   reasonForm: FormGroup;
 
   constructor(public activeModal: NgbActiveModal, private fb: FormBuilder) {
+    super();
     this.reasonForm = this.fb.group({
       reason: new ExtendedFormControl('', [Validators.required, Validators.maxLength(250)], 'reason'),
       className: 'reason'
@@ -23,7 +25,7 @@ export class ReasonModalComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onSubmit(status) {
+  onSubmit() {
     this.updateStatus.emit({title: this.title, formObject: this.reasonForm})
   }
 }
