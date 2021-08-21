92b26ae2bc852f26c0a0e40a1d6c25372a3d264c
P.Harshavardhan <harshavardhan98@yahoo.co.in>
2021-08-21 14:41:20 +0530

    Added Range date-picker component

--- src/app/pages/platform/private/shop-list/shop-list.component.ts
+++ src/app/pages/platform/private/shop-list/shop-list.component.ts
@@ -136,34 +136,6 @@ export class ShopListComponent implements OnInit {
     this.getShopList();
   }
 
-  onDateSelection(date: NgbDate) {
-    if (!this.fromDate && !this.toDate) {
-      this.fromDate = date;
-    } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
-      this.toDate = date;
-    } else {
-      this.toDate = null;
-      this.fromDate = date;
-    }
-  }
-
-  isHovered(date: NgbDate) {
-    return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
-  }
-
-  isInside(date: NgbDate) {
-    return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
-  }
-
-  isRange(date: NgbDate) {
-    return date.equals(this.fromDate) || (this.toDate && date.equals(this.toDate)) || this.isInside(date) || this.isHovered(date);
-  }
-
-  validateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
-    const parsed = this.formatter.parse(input);
-    return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
-  }
-
   onClick(event) {
      if(event.type == 'click') {
         this.router.navigateByUrl(APP_ROUTES.SHOP + "/" + event.row.id);
